bca7c879055f896102811163d3e8ec85386e0e23
Stefan Scherer <scherer_stefan@icloud.com>
2018-11-26 14:54:41 +0100

    Use 64bit parameters only

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,7 @@
 $packageName = 'manifest-tool'
-$url = 'https://github.com/estesp/manifest-tool/releases/download/v0.9.0/manifest-tool-windows-amd64.exe'
-$url64 = $url
-$checksum = 'c34688e5b074eaa36a885a19f96e8d4f2a4e29fd4ed8ccac5d2fc3049ba61ae6'
-$checksum64 = $checksum
-$checksumType = 'sha256'
-$checksumType64 = $checksumType
+$url64 = 'https://github.com/estesp/manifest-tool/releases/download/v0.9.0/manifest-tool-windows-amd64.exe'
+$checksum64 = 'c34688e5b074eaa36a885a19f96e8d4f2a4e29fd4ed8ccac5d2fc3049ba61ae6'
+$checksumType64 = 'sha256'
 
 $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $file = "$($destination)\manifest-tool.exe"
@@ -14,9 +11,6 @@ if (![System.IO.Directory]::Exists($destination)) {[System.IO.Directory]::Create
 Get-ChocolateyWebFile `
   -PackageName    $packageName `
   -FileFullPath   $file `
-  -Url            $url `
   -Url64bit       $url64 `
-  -Checksum       $checksum `
   -Checksum64     $checksum64 `
-  -ChecksumType   $checksumType `
   -ChecksumType64 $checksumType64
